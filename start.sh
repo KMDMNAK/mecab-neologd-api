aefbe97d26c564328cc25a5c5fa5099da4113f6b
KMDMNAK <kmdmuneaki@gmail.com>
2021-07-03 16:45:21 +0900

    Adjust dev configs

--- start.sh
+++
@@ -1,9 +0,0 @@
-CONTAINER_NAME=mecab-api
-if [ $MODE = DEVELOPMENT ]; then
-    docker build -t $CONTAINER_NAME .
-elif [ $MODE = RUN ]; then
-    docker run -it -v $PWD/src/:/go/src/ $CONTAINER_NAME /bin/bash
-else
-    echo "Not DEVELOPMENT"
-fi
-docker run -it -v $PWD/src/:/go/src/ $CONTAINER_NAME /bin/bash
